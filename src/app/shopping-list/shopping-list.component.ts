027f5deda81c1150a23abaeacfaa3aad76eb56e8
MortezaFathnia <fathniamorteza@gmail.com>
2019-02-27 22:47:27 +0330

    firstcommit

---
+++ src/app/shopping-list/shopping-list.component.ts
@@ -0,0 +1,18 @@
+import { Component, OnInit } from '@angular/core';
+import {Ingrediant} from '../shared/ingrediant.model'
+@Component({
+  selector: 'app-shopping-list',
+  templateUrl: './shopping-list.component.html',
+  styleUrls: ['./shopping-list.component.css']
+})
+export class ShoppingListComponent implements OnInit {
+  Ingrediants: Ingrediant []= [
+    new Ingrediant('Apple', 5 ),
+    new Ingrediant('Tomatoes', 10)
+  ];
+  constructor() { }
+
+  ngOnInit() {
+  }
+
+}
